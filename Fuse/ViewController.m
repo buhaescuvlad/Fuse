8e92bcc15f13f865762dcdd994817517641209ba
Vlad - Constantin <buhaescuvlad@yahoo.com>
2015-10-21 18:18:44 +0100

    initial cimmit] as

--- Fuse/ViewController.m
+++ Fuse/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //here we do the job
 }
 
 - (void)didReceiveMemoryWarning {
